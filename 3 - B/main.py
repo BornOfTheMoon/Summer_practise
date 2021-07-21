6c920b031a3a16b0e976556af667b6463074a31f
BornOfTheMoon <83153544+BornOfTheMoon@users.noreply.github.com>
2021-07-21 21:33:57 +1000

    changed size in 3-b

--- 3 - B/main.py
+++ 3 - B/main.py
@@ -2,11 +2,11 @@ import pygame as pg
 
 
 pg.init()
-size = 700, 500
+size = 300, 300
 screen = pg.display.set_mode(size)
 running = True
 screen.fill((0, 0, 0))
-pg.draw.rect(screen, (0, 0, 255), (0, 0, 100, 100))
+pg.draw.rect(screen, (0, 0, 255), (0, 0, 50, 50))
 pg.display.flip()
 
 while running:
@@ -19,7 +19,7 @@ while running:
                 for second_event in pg.event.get():
                     if second_event.type == pg.MOUSEMOTION:
                         screen.fill((0, 0, 0))
-                        pg.draw.rect(screen, (0, 0, 255), (second_event.pos[0] - 50, second_event.pos[1] - 50, 100, 100))
+                        pg.draw.rect(screen, (0, 0, 255), (second_event.pos[0] - 25, second_event.pos[1] - 25, 50, 50))
                     if second_event.type == pg.MOUSEBUTTONUP or event.type == pg.QUIT:
                         moving = False
                 pg.display.flip()
