b5554f20de3b4cb0b418bb6bb6fd3c4b6712ffd0
BornOfTheMoon <83153544+BornOfTheMoon@users.noreply.github.com>
2021-07-27 23:03:34 +1000

    changed sizes and add turning to unicorn

--- project_on_pygame/main.py
+++ project_on_pygame/main.py
@@ -10,13 +10,13 @@ INTRO_TEXT = ["Играть", "",
               "Правила", "",
               "Выход"]
 RULES_TEXT = ["""Помогите единорогу выбраться из замка.""",
-                  """Для этого на каждом уровне нужно пройти""",
-                  """через дверь, но она откроется только после """,
-                  """того, как единорог встанет на кнопку.""",
-                  """Помогайте ему с помощью клавиш "вправо" """,
-                  """и "влево" для движения и клавиши "вверх" """,
-                  """для прыжка.""",
-                  "Удачи! Единорог надеется на Вас!"]
+              """Для этого на каждом уровне нужно пройти""",
+              """через дверь, но она откроется только после """,
+              """того, как единорог встанет на кнопку.""",
+              """Помогайте ему с помощью клавиш "вправо" """,
+              """и "влево" для движения и клавиши "пробел" """,
+              """для прыжка.""",
+              "Удачи! Единорог надеется на Вас!"]
 BACK_TEXT = ["Перейти в меню"]
 LOSE_TEXT = ["О нет!", "Единорог остался в замке!"]
 LOSE_MENU = ["Попробовать ещё", "", "Перейти в меню"]
@@ -28,8 +28,12 @@ COUNT_LEVELS = 2
 
 pygame.init()
 size = WIDTH, HEIGHT = 1100, 700
-tile_width = tile_height = 110
+spike_width = lever_width = unicorn_width = unicorn_height = tile_width = tile_height = 110
+spike_height = lever_height = 50
 tile_size = tile_width, tile_height
+spike_size = spike_width, spike_height
+lever_size = lever_width, lever_height
+unicorn_size = unicorn_width, unicorn_height
 screen = pygame.display.set_mode(size)
 FPS = 60
 clock = pygame.time.Clock()
@@ -70,7 +74,6 @@ tile_images = {
     'grass': load_image('grass.jpg'),
     'spikes': load_image('spike.jpg', -1)
 }
-# player_sprite = load_image('unicorn.png', -1)
 
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
@@ -90,14 +93,17 @@ pygame.draw.rect(dark_background, (0, 0, 0, 200), (0, 0, WIDTH, HEIGHT))
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
+        sprite_size = tile_size
         if tile_type == 'grass':
             super().__init__(tiles_group, all_sprites, grasses_group)
         elif tile_type == 'spikes':
             super().__init__(tiles_group, all_sprites, spikes_group)
+            sprite_size = spike_size
         else:
             super().__init__(tiles_group, all_sprites)
-        self.image = pygame.transform.scale(tile_images[tile_type], tile_size)
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y + 20)
+        self.image = pygame.transform.scale(tile_images[tile_type], sprite_size)
+        dif_height = tile_height + 20 - sprite_size[1]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y + dif_height)
         self.pos = pos_x, pos_y
         self.tile_type = tile_type
 
@@ -123,14 +129,15 @@ class Lever(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(levers_group, all_sprites)
         self.sprite = load_image('inactive_lever.jpg', -1)
-        self.image = pygame.transform.scale(self.sprite, tile_size)
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y + 20)
+        self.image = pygame.transform.scale(self.sprite, lever_size)
+        dif_height = tile_height + 20 - lever_size[1]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y + dif_height)
         self.pos = pos_x, pos_y
         self.state = False
 
     def update(self):
         self.sprite = load_image('active_lever.jpg', -1)
-        self.image = pygame.transform.scale(self.sprite, tile_size)
+        self.image = pygame.transform.scale(self.sprite, lever_size)
         doors_group.draw(screen)
         pygame.display.flip()
         self.state = True
@@ -140,11 +147,12 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.sprite = load_image('unicorn_r.png', -1)
-        self.image = pygame.transform.scale(self.sprite, tile_size)
+        self.image = pygame.transform.scale(self.sprite, unicorn_size)
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y + 20)
         self.pos = pos_x, pos_y
         self.on_ground = True
         self.gravity = 0.05
+        self.vision = 1
 
     def move(self, direction, width, height):
         m_x, m_y = self.pos
@@ -188,6 +196,19 @@ class Player(pygame.sprite.Sprite):
             self.pos = m_x, m_y
             x, y = self.rect.x, self.rect.y + self.gravity * tile_height
             self.rect = self.image.get_rect().move(x, y)
+
+    def turn(self, direction):
+        if direction == 1 and self.vision == -1:
+            self.sprite = load_image('unicorn_r.png', -1)
+            self.vision = 1
+        elif direction == -1 and self.vision == 1:
+            self.sprite = load_image('unicorn_l.png', -1)
+            self.vision = -1
+        else:
+            return
+        self.image = pygame.transform.scale(self.sprite, unicorn_size)
+        player_group.draw(screen)
+        pygame.display.flip()
 
 
 def generate_level(level, width, height):
@@ -359,8 +380,12 @@ def play(number=1):
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     player.move((-1, 0), level_width, level_height)
+                    if player.vision == 1:
+                        player.turn(-1)
                 elif event.key == pygame.K_RIGHT:
                     player.move((1, 0), level_width, level_height)
+                    if player.vision == -1:
+                        player.turn(1)
                 elif event.key == pygame.K_SPACE:
                     player.move((0, -1), level_width, level_height)
 
